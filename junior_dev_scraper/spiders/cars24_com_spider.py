186af6673411697f9fb314070814322444ce2b97
walvesnl <w.alvesnl@gmail.com>
2023-10-02 22:27:38 +0200

    spider crawling engine

--- junior_dev_scraper/spiders/cars24_com_spider.py
+++ junior_dev_scraper/spiders/cars24_com_spider.py
@@ -9,17 +9,52 @@ from w3lib.url import add_or_replace_parameter
 
 class Car24ComSpider(scrapy.Spider):
     name = 'cars24_com_spider'
-    start_urls = ['https://www.cars24.com/ae/buy-used-cars-dubai/']
+    api_url = 'https://listing-service.c24.tech/v2/vehicle?isSeoFilter=true&sf=city:DU&sf=gaId:1120805531.1696266648&size=25&spath=buy-used-cars-dubai&page={}&variant=filterV5'
+    start_page = 0
+    catalog_headers = {
+        'X_country': 'AE',
+        'X_vehicle_type': 'CAR'
+    }
+    
+    total_products = 0
+
+    def start_requests(self):
+        yield scrapy.Request(self.api_url.format(self.start_page), headers=self.catalog_headers, callback=self.parse)
 
     def parse(self, response):
-        for products in response.css('a._1Lu5u'):
+        data = json.loads(response.text)
+        
+        products = data.get('results', [])
+       
+
+        for product in products:
+            
+            brand = product.get('make')
+            model = product.get('model')
+            year = product.get('year')
+            city = product.get('city')
+            appointment_id = product.get('appointmentId')
+            engine = product.get('engineSize')
+            price = product.get('price')
+            model = product.get('model')
+            modelParsed = "-".join(word.lower() for word in model.split())
+            mileage = product.get('odometerReading')
+            link = f'https://www.cars24.com/ae/buy-used-{brand.lower()}-{modelParsed}-{year.lower()}-cars-{city.lower()}-{appointment_id.lower()}'
+
             yield {
-                'brand': products.css('h3.RZ4T7::text').get().split(' ', 1)[0],
-                'engine': products.css('ul._3ZoHn li:nth-of-type(3)::text').get(),
-                'year': products.css('._1i1E6::text').get().split(' |', 1)[0],
-                'link': products.attrib['href'],
-                'price': products.css('.aApXW ._7yds2::text').get().replace(',', ''),
-                'model': products.css('h3.RZ4T7::text').get().split(' ', 1)[1],
-                'mileage': products.css('ul._3ZoHn li:nth-of-type(2)::text').get().replace(',', '').replace(' km', ''),
+                'brand': brand,
+                'engineSize': engine,
+                'year': year,
+                'link': link,
+                'price': price,
+                'model': model,
+                'mileage': mileage,
             }
+            self.total_products += 1
+            
+        
+        if self.total_products < data['total']:
+            self.start_page += 1
+            yield scrapy.Request(self.api_url.format(self.start_page), headers=self.catalog_headers, callback=self.parse)
     # Good luck
+
