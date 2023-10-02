16d87f2af67336167f230e83fe00fea4d8ecbb57
walvesnl <w.alvesnl@gmail.com>
2023-10-02 20:38:30 +0200

    first fields

--- junior_dev_scraper/spiders/cars24_com_spider.py
+++ junior_dev_scraper/spiders/cars24_com_spider.py
@@ -9,5 +9,17 @@ from w3lib.url import add_or_replace_parameter
 
 class Car24ComSpider(scrapy.Spider):
     name = 'cars24_com_spider'
+    start_urls = ['https://www.cars24.com/ae/buy-used-cars-dubai/']
 
+    def parse(self, response):
+        for products in response.css('a._1Lu5u'):
+            yield {
+                'brand': products.css('h3.RZ4T7::text').get().split(' ', 1)[0],
+                'engine': products.css('ul._3ZoHn li:nth-of-type(3)::text').get(),
+                'year': ,
+                'link': ,
+                'price': ,
+                'model': ,
+                'mileage': ,
+            }
     # Good luck
