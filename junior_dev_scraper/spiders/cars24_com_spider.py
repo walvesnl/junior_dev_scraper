5935ef979c941da9c2a11d4b63f12613fa991c30
walvesnl <w.alvesnl@gmail.com>
2023-10-02 22:59:48 +0200

    spider complete

--- junior_dev_scraper/spiders/cars24_com_spider.py
+++ junior_dev_scraper/spiders/cars24_com_spider.py
@@ -1,14 +1,12 @@
 import scrapy
 import json
-import re
-from benedict import benedict
-from w3lib.url import add_or_replace_parameter
-
-# See https://www.cars24.com/ae/buy-used-cars-dubai/
-
 
 class Car24ComSpider(scrapy.Spider):
     name = 'cars24_com_spider'
+    custom_settings = {
+        'FEED_URI': 'output_file.csv',  # Specify the file name
+        'FEED_FORMAT': 'csv',  # Specify the output format
+    }
     api_url = 'https://listing-service.c24.tech/v2/vehicle?isSeoFilter=true&sf=city:DU&sf=gaId:1120805531.1696266648&size=25&spath=buy-used-cars-dubai&page={}&variant=filterV5'
     start_page = 0
     catalog_headers = {
@@ -31,24 +29,26 @@ class Car24ComSpider(scrapy.Spider):
             
             brand = product.get('make')
             model = product.get('model')
+            brandParsed = "-".join(word.lower() for word in brand.split())
+            modelParsed = "-".join(word.lower() for word in model.split())
             year = product.get('year')
             city = product.get('city')
+            cityParsed = "-".join(word.lower() for word in city.split())
             appointment_id = product.get('appointmentId')
             engine = product.get('engineSize')
             price = product.get('price')
-            model = product.get('model')
-            modelParsed = "-".join(word.lower() for word in model.split())
+            
             mileage = product.get('odometerReading')
-            link = f'https://www.cars24.com/ae/buy-used-{brand.lower()}-{modelParsed}-{year.lower()}-cars-{city.lower()}-{appointment_id.lower()}'
+            link = f'https://www.cars24.com/ae/buy-used-{brandParsed}-{modelParsed}-{year.lower()}-cars-{cityParsed}-{appointment_id.lower()}'
 
             yield {
                 'brand': brand,
                 'engineSize': engine,
                 'year': year,
-                'link': link,
                 'price': price,
                 'model': model,
                 'mileage': mileage,
+                'link': link,
             }
             self.total_products += 1
             
@@ -56,5 +56,3 @@ class Car24ComSpider(scrapy.Spider):
         if self.total_products < data['total']:
             self.start_page += 1
             yield scrapy.Request(self.api_url.format(self.start_page), headers=self.catalog_headers, callback=self.parse)
-    # Good luck
-
